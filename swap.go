97ade07f73ea1981d80ca76ce9ce5a4823c75646
emin790 <71364869+emin790@users.noreply.github.com>
2020-09-20 21:18:33 +0300

    swap_ver2
    замена двух переменных местами через сумму и разность

--- swap.go
+++ swap.go
@@ -2,22 +2,15 @@ package main
 
 import "fmt"
 
-func swap(a,b *int){					//функция принимает указатели a и b
-	*a = *b
-	*b = *a
-}
-
 func main() {
 	
-	a := 111
-	b := 222
-	
-	fmt.Println("До замены:")
+	a := 9
+	b := 8
 	fmt.Println("a =", a, "b =", b)
 	
-	swap(&a,&b) 						//передаем в функцию адреса a и b
+	a = a + b  // сохраняем сумму a и b в a
+	b = a - b  // вычитаем из суммы b, это будет прежднее значение a. в a все еще хранится сумма
+	a = a - b 
 	
-	fmt.Println("После замены:")
 	fmt.Println("a =", a, "b =", b)
-
 }
